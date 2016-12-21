88474ac2fc075b1169f7f5e6c21e276cbe7b4ee2
marcsnts <msanto03@mail.uoguelph.ca>
2016-12-21 13:58:31 -0500

    Made corresponding drink classes

---
+++ Coffee Machine/Beverages.swift
@@ -0,0 +1,81 @@
+//
+//  Beverages.swift
+//  Coffee Machine
+//
+//  Created by Marc Santos on 2016-12-21.
+//  Copyright © 2016 Marc Santos. All rights reserved.
+//
+
+import Foundation
+
+enum HotDrink {
+    case Coffee, HotChocolate, Cappuccino
+}
+
+enum BeverageSize {
+    case Small, Medium, Large
+}
+
+class Beverage: NSObject {
+    
+    var size: BeverageSize
+    
+    init(size: BeverageSize) {
+        self.size = size
+    }
+    
+    
+}
+
+class Coffee: Beverage {
+    
+    var sugar: Int
+    
+    init(size: BeverageSize, sugar: Int) {
+        self.sugar = sugar
+        super.init(size: size)
+    }
+    
+    func addSugar() {
+        self.sugar = self.sugar+1
+    }
+    
+    func removeSugar() {
+        guard self.sugar > 0 else {
+            print("No sugar to remove")
+            return
+        }
+        self.sugar = self.sugar-1
+    }
+}
+
+class HotChocolate: Beverage {
+    
+    var whippedCream: Bool
+    
+    init(size: BeverageSize, whippedCream: Bool) {
+        self.whippedCream = whippedCream
+        super.init(size: size)
+    }
+    
+    func addCream() {
+        self.whippedCream = true
+    }
+    
+    func removeCream() {
+        self.whippedCream = false
+    }
+    
+}
+
+class Cappuccino: Beverage {
+    
+}
+
+
+
+
+
+
+
+
