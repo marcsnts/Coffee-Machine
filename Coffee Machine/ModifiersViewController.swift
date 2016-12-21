89b7338fc9d452d7aeddfed039d304f60d5c8ed1
marcsnts <msanto03@mail.uoguelph.ca>
2016-12-21 14:34:12 -0500

    Added Eureka and assets

---
+++ Coffee Machine/ModifiersViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ModifiersViewController.swift
+//  Coffee Machine
+//
+//  Created by Marc Santos on 2016-12-21.
+//  Copyright © 2016 Marc Santos. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ModifiersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
