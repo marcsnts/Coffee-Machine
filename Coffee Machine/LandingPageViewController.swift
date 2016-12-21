492bc3b5e5b9bedd9429b6f81b2bddd9ecdb73e1
marcsnts <msanto03@mail.uoguelph.ca>
2016-12-21 14:03:37 -0500

    Setup SnapKit dependancy

---
+++ Coffee Machine/LandingPageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LandingPageViewController.swift
+//  Coffee Machine
+//
+//  Created by Marc Santos on 2016-12-21.
+//  Copyright © 2016 Marc Santos. All rights reserved.
+//
+
+import UIKit
+
+class LandingPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- Coffee Machine/ViewController.swift
+++ Coffee Machine/ViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        Coffee
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
