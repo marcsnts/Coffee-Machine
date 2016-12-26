1a55d8a0a9b9c36845116242290069307a52289d
marcsnts <msanto03@mail.uoguelph.ca>
2016-12-26 03:00:02 -0500

    Custom select row

--- Coffee Machine/ModifiersViewController.swift
+++ Coffee Machine/ModifiersViewController.swift
@@ -14,7 +14,23 @@ class ModifiersViewController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupForm()
+    }
+    
+    private func setupForm() {
+        
+        tableView?.snp.makeConstraints { (make) -> Void in
+            make.edges.equalTo(self.view)
+        }
+        
+        form = Section()
+            <<< SelectRow() {
+                $0.title = "swegg"
+                $0.value = false
+                }.onCellSelection { cell, row in
+                    cell.update()
+            }
+        +++ Section()
     }
 
 }
---
+++ Coffee Machine/SelectRow.swift
@@ -0,0 +1,61 @@
+//
+//  SelectRow.swift
+//  Coffee Machine
+//
+//  Created by Marc Santos on 2016-12-26.
+//  Copyright © 2016 Marc Santos. All rights reserved.
+//
+
+import Foundation
+import Eureka
+import UIKit
+import SnapKit
+
+public class SelectCell : Cell<Bool>, CellType {
+  
+    let title = UILabel()
+    private var isSettingUp = true
+    
+    public override func setup() {
+    
+        super.setup()
+        selectionStyle = .none
+        setupCell()
+        
+    }
+    
+    public override func update() {
+        
+        let selectedColor = UIColor.cyan
+        
+        if !isSettingUp {
+            row.value = row.value == true ? false : true
+        }
+        else {
+            isSettingUp = false
+        }
+
+        backgroundColor = row.value == true ? selectedColor : UIColor.clear
+        
+    }
+    
+    private func setupCell() {
+
+        title.text = row.title
+        
+        self.addSubview(title)
+        
+        title.snp.makeConstraints { (make) -> Void in
+            make.center.equalToSuperview()
+        }
+        
+    }
+
+}
+
+public final class SelectRow: Row<SelectCell>, RowType {
+    required public init(tag: String?) {
+        super.init(tag: tag)
+        cellProvider = CellProvider<SelectCell>(nibName: "SelectCell")
+    }
+}
